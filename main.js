a40441aeb89b61b7d307c45cc914be4c5e5e5ce7
spicyduck1 <ryan.k.v4b@gmail.com>
2025-06-18 13:11:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
-// Voorbeeld jQuery code
+// Wacht tot het document klaar is (met jQuery)
 $(document).ready(function() {
-   console.log("jQuery is geladen!");
+    console.log("jQuery is geladen en ready!");
 
-   // Vis.js netwerk visualisatie
-   var nodes = new vis.DataSet([
-       {id: 1, label: 'Node 1'},
-       {id: 2, label: 'Node 2'},
-       {id: 3, label: 'Node 3'}
-   ]);
+    // Definieer de knooppunten voor het netwerk
+    var nodes = new vis.DataSet([
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'}
+    ]);
 
-   var edges = new vis.DataSet([
-       {from: 1, to: 2},
-       {from: 1, to: 3}
-   ]);
+    // Definieer de verbindingen (edges) tussen de knooppunten
+    var edges = new vis.DataSet([
+        {from: 1, to: 2},
+        {from: 1, to: 3}
+    ]);
 
-   var container = document.getElementById('network');
-   var data = { nodes: nodes, edges: edges };
-   var options = {};
-   var network = new vis.Network(container, data, options);
+    // Maak een container voor het netwerk
+    var container = document.getElementById('network');
+
+    // De data voor het netwerk (knooppunten en verbindingen)
+    var data = { nodes: nodes, edges: edges };
+
+    // Opties voor het netwerk (bijvoorbeeld instellingen voor weergave)
+    var options = {};
+
+    // Maak het netwerk aan
+    var network = new vis.Network(container, data, options);
 });
